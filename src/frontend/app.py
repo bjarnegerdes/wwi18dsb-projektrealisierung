b3cbcb102d75b0785eb8db8e67074c78c881089a
Johannes998 <johannes.deufel@gmx.de>
2021-06-12 15:33:53 +0200

    Added averaged sentiment visualization of multiple tickers

--- src/frontend/app.py
+++ src/frontend/app.py
@@ -1,8 +1,26 @@
 from flask import Flask, url_for, request, render_template, redirect
-from sqlalchemy import create_engine
 import pandas as pd
 import time
-from datetime import datetime
+import datetime
+from sqlalchemy.orm import Session
+from finviz.screener import Screener
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import Boolean, Column, DateTime, Float, String, create_engine, or_, and_
+from pathlib import Path
+import os
+
+#Create Dataclass for Redditpostes
+Base = automap_base()
+metadata = Base.metadata
+
+class Redditpost(Base):
+    __tablename__ = 'redditposts'
+
+    ticker = Column(String, primary_key=True, nullable=False)
+    created_utc = Column(DateTime, primary_key=True, nullable=False)
+    comment = Column(String, nullable=False)
+    passed_filter_checks = Column(Boolean)
+    sentiment = Column(Float(53))
 
 # Connect to database
 PGHOST='h2933354.stratoserver.net'
@@ -11,8 +29,35 @@ PGUSER='admin'
 PGPASSWORD='password'
 PGPORT='3000'
 
-connection_string = 'postgresql://'+PGUSER+':'+PGPASSWORD+'@'+PGHOST+':'+PGPORT+'/'+PGDATABASE
-engine = create_engine(connection_string)
+engine = create_engine('postgresql://'+PGUSER+':'+PGPASSWORD+'@'+PGHOST+':'+PGPORT+'/'+PGDATABASE)
+conn = engine.connect()
+
+Base.prepare(engine, reflect=True)
+Base.metadata.create_all(bind=engine)
+session = Session(engine)
+
+# Extrahieren aller Ticker in der DB um mit diesen später zu filtern
+ticker_obj = session.query(Redditpost.ticker).distinct().all()
+ticker = [ticker[0] for ticker in ticker_obj]
+
+# Update metadata if it is older than 24 hours
+delta = datetime.datetime.utcnow() - datetime.datetime.strptime(str(list(Path(".").rglob("metadata_*.csv"))[0])[-30:-4], '%Y-%m-%d %H:%M:%S.%f')
+if delta.days == 0:
+    df_data = pd.read_csv(str(list(Path(".").rglob("metadata_*.csv"))[0]), index_col=False)
+else:
+    stock_list_performance = Screener(table='Performance', order='price', filters = ['cap_midover', 'ipodate_more1', 'exch_nasd'])
+    stock_list_overview = Screener(table='Overview', order='price', filters = ['cap_midover', 'ipodate_more1', 'exch_nasd'])
+    # Daten lesen als Dataframe und mergen
+    df_performance = pd.DataFrame(stock_list_performance.data)
+    df_overview = pd.DataFrame(stock_list_overview.data)
+
+    difference_cols = list(df_performance.columns.difference(df_overview.columns))
+    difference_cols.append("Ticker")
+
+    df_data = df_overview.merge(df_performance[difference_cols], left_on="Ticker", right_on="Ticker", how="outer")
+
+    os.remove(str(list(Path(".").rglob("metadata_*.csv"))[0]))
+    df_data.to_csv(f"metadata_{datetime.datetime.utcnow()}.csv", index=False)
 
 
 # app = Flask(__name__) creates an instance of the Flask class called app. 
@@ -41,78 +86,16 @@ def index():
 # This function returns the preferences.html page which is used to input a text which should be analyzed.
 @app.route("/preferences")
 def preferences():
-    return render_template('preferences.html')
+    return render_template('preferences.html', sectors=sorted(list(set(df_data['Sector']))))
 
 # This function is used to pass the text input to the machine learning classification model and calculate the results if we are only using the clustering based on text features. 
 @app.route("/preferences_end", methods=['GET', 'POST'])
 def preferences_end():
     if request.method == 'POST':
 
-        # Get the text input and save it globally as we will need it in another function
-        global text
-        text = "bla"
+        global sectors
+        sectors = [sector.replace("_"," ") for sector in request.form.getlist("sector")]
 
-        # Get the taget variable
-        global targetvariable
-
-        targetvariable = "No"
-
-        # If no target variable for an improved classification was submitted, then redirect directly to the result
-
-        if targetvariable=='No':
-
-            return redirect(url_for('dashboard'))
-        
-        # Otherwise ask for the other variables
-
-        else:
-
-            return redirect(url_for('variableinput'))
-
-# This function returns the variableinput.html page which is used to input the values of the additional variables.
-@app.route("/variableinput")
-def variableinput():
-
-    # We are definig all possible target variables and removing the choosen one to ask for the others in the frontend.
-    neededvariables = ['Age', 'Gender', 'Topic', 'Sign']
-    neededvariables.remove(targetvariable)
-
-    return render_template('variableinput.html', targetvariable=targetvariable, neededvariables=neededvariables)
-
-# This function is used to pass the text input and the other submitted variables to the machine learning classification model and calculate the results. 
-@app.route("/variableinput_end", methods=['GET', 'POST'])
-def variableinput_end():
-    if request.method == 'POST':
-
-        # Define the labels as global as we need them in another function.
-        global age, gender, topic, sign, clustering
-        
-        # Ask for the variables whech need to be presubmitted
-        try:
-            age = int(request.form['age'])
-        except:
-            age = 0
-
-        try:
-            gender = request.form['gender']
-        except:
-            gender = '0'
-
-        try:
-            topic = request.form['topic']
-        except:
-            topic = '0'
-
-        try:
-            sign = request.form['sign']
-        except:
-            sign = '0'
-
-        # Now we are starting the text analyzation and produce a result.
-        #age, gender, sign, topic, clustering = randomModel(text, numerical=True, input=[age, gender, sign, topic], target=targetvariable) #Model for stacked prediction
-
-        # On the Website we use pictures for the result of age which is created now.
-        #center_text(str(age), 'age')
         return redirect(url_for('dashboard'))
 
 # This function returns the calculated labels to the html templates. 
@@ -120,14 +103,57 @@ def variableinput_end():
 @app.route("/dashboard")
 def dashboard():
     try:
-        company='EBAY'
-        sql_data = pd.read_sql(f"SELECT created_utc as time, sentiment FROM redditposts WHERE ticker = '{company}' AND passed_filter_checks = true ORDER BY 1", con=engine).dropna()
-        #data = ["{"+f"x: {int(time.mktime(row[1]['time'].to_pydatetime().timetuple()))*1000}, y: {str(row[1]['sentiment'])}"+"}, " for row in data.iterrows()]
-        data = [float(row[1]['sentiment']) for row in sql_data.iterrows()]
-        labels = [int(time.mktime(row[1]['time'].to_pydatetime().timetuple()))*1000 for row in sql_data.iterrows()]
-        return render_template('dashboard.html', data=data, labels=labels, company=company)
+        #Retrieve all tickers which are relevant for the customer sector preferences
+        relevant_tickers = df_data[(df_data["Sector"].isin(sectors))]["Ticker"]
+
+        #Read the relevant tickers from the SQL database
+        tickerdata = pd.DataFrame(session.query(Redditpost.ticker, Redditpost.created_utc, Redditpost.sentiment).filter(and_(Redditpost.ticker.in_(relevant_tickers), Redditpost.sentiment != None)).all(),\
+                  columns=["Ticker", "created_utc", "sentiment"]).sort_values("created_utc")
+
+        #Join the tickers with the sector
+        tickerdata = pd.merge(tickerdata, df_data, on=["Ticker", "Ticker"], how="left", indicator=True
+            ).query('_merge=="both"')[["Ticker","created_utc","sentiment","Sector"]]
+        
+        #Define colors used in the dashboard
+        rgb=["rgba(152, 176, 155, 0.8)", "rgba(8, 103, 131, 0.8)", "rgba(146, 140, 138, 0.8)", "rgba(192, 192, 153, 0.8)", "rgba(59, 60, 59, 0.8)", "rgba(71, 173, 207, 0.8)", "rgba(99, 136, 104, 0.8)", "rgba(8, 75, 96, 0.8)", "rgba(8, 50, 62, 0.8)", "rgba(192, 226, 238, 0.8)"]      
+        
+        #Aggregate data per ticker
+        series_collection = []
+        counter = 0
+        for company in set(tickerdata["Ticker"]):
+
+            # Extract the data for each each ticker
+            df = tickerdata[tickerdata['Ticker'] == company]
+
+            # Read the sector of the ticker
+            sector_new = list(df_data[(df_data["Ticker"] == company)]["Sector"])[0]
+
+            # Add a current timestamp with sentiment 0 to ensure that the time series of every ticker has same length
+            df = df.append(pd.DataFrame({'Ticker':[company],'created_utc':[datetime.datetime.utcnow()],'sentiment':[0],'Sector':[sector_new]}), ignore_index=False)
+
+            # Calculate average sentiment per 6 hours and calculate the rolling average based on the 6-hour-average
+            resampled_ticker = df.resample('6h', on='created_utc').sentiment.mean().fillna(0).rolling('24h').mean()
+
+            # Safe for each ticker: (newest 125 sentiment 6-hour-time-series-intervalls, comany and sector, used line color)
+            series_collection.append((list(resampled_ticker)[-125:], company + ", " + sector_new, rgb[counter]))
+
+            #Allows repaeting the colors (we use 10 different colors)
+            counter +=1
+            if counter == 10:
+                counter = 0
+
+
+        #resampled_ticker = tickerdata.resample('6H', on='created_utc', offset='0Min0s').sentiment.mean().fillna(method='ffill')
+        #tickerdata = tickerdata.set_index("created_utc").sort_index()
+        
+
+        # Combining the time stamps with the sentiment data
+        sentiment_data = [[int(date/1000000) for date in resampled_ticker.index.values.tolist()][-125:], series_collection]
+        #sentiment_data = [[int(date/1000000) for date in tickerdata.index.values.tolist()], list(tickerdata["sentiment"].rolling('24h').mean())]
+        
+        return render_template('dashboard.html', sentiment_data=sentiment_data)
     except NameError:
-        return render_template('preferences.html')
+        return render_template('preferences.html', sectors=sorted(list(set(df_data['Sector']))))
 
 # This function returns the team.html page containing the information details from our team.
 @app.route("/team")
@@ -139,4 +165,4 @@ def team():
 # In our case we are executing the script. Therefore, __name__ will be equal to "__main__". 
 # That means the if conditional statement is satisfied and the app.run() method will be executed.
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
